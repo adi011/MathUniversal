3b3ea8f2cdf0b7813dce2ea5ac071a590a9d844f
Michał Bartkiewicz <michal4421@outlook.com>
2015-09-05 15:29:02 +0200

    Introduce to StartViewModel and ... CALINECZKA :) :). i will finish when I back

---
+++ MathUniversal/ViewModel/StartViewModel.cs
@@ -0,0 +1,54 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUniversal.ViewModel
+{
+    public class StartViewModel:ViewModelBase
+    {
+
+        public StartViewModel()
+        {
+           // navigateToExpressions = new RelayCommand(Calineczka1);
+            //navigateToMatrix = new RelayCommand(Calineczka2);
+            //navigateToTheSystemOfEquations = new RelayCommand(Calineczka3)
+        }
+
+        private RelayCommand navigateToExpressions;
+        public RelayCommand NavigateToExpressions
+        {
+            get
+            {
+                return navigateToExpressions;
+            }
+        }
+
+
+
+        private RelayCommand navigateToMatrix;
+        public RelayCommand NavigateToMatrix
+        {
+            get
+            {
+                return navigateToMatrix;
+            }
+        }
+        private RelayCommand navigateToTheSystemOfEquations;
+        public RelayCommand NavigateToTheSystemOfEquations
+        {
+            get
+            {
+                return navigateToTheSystemOfEquations;
+            }
+        }
+
+
+
+
+
+    }
+}
