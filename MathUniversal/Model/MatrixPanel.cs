a83a2b4e9cc9f5b7198bd8742c1f4ef2944f0cb2
Michał Bartkiewicz <michal4421@outlook.com>
2015-09-11 11:57:02 +0200

    Add MatrixPanel and introducy implement

---
+++ MathUniversal/Model/MatrixPanel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUniversal.Model
+{
+    class MatrixPanel:ObservableObject
+    {
+        public MatrixPanel()
+        {
+
+        }
+        private string _matrixHeight;
+        public string MatrixHeight
+        {
+            get
+            {
+                return _matrixHeight;
+            }
+        }
+        private string _matrixWidth;
+        public string MatrixWidth
+        {
+            get
+            {
+                return _matrixWidth;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
